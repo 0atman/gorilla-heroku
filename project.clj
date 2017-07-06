c9e3c2f8fe21288126e610f50fa31f3005579577
Tristram Oaten <o@tman.me>
2017-07-06 12:44:32 +0100

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject gorilla-test "0.1.0-SNAPSHOT"
   :description "A test project for Gorilla REPL."
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]]
   :main ^:skip-aot gorilla-test.core
   :target-path "target/%s"
   :plugins [[lein-gorilla "0.4.0"]]
